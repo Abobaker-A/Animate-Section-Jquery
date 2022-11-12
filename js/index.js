6f7cd3234df324bd3334842f0d5a544c11af39fc
Abobaker-A <abubakrr96@gmail.com>
2022-11-12 19:57:10 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('#WORK').animate({width:'100%'},2000 , function(){
+    $('#WORK').animate( { "width" : "100%" , "height" : "1000px" }, 2000 , function(){
+        $('.title').slideDown(1000,function(){
+            $('.hhhhh').show(2000 , function(){
+                $('.container').slideDown(2000)
+            })
+        
+            })
+    }
+
+)})
